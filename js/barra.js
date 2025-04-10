4702d86bd943fcb3da4ff2d46037264552cd5696
dientecito <haritz.argentina@gmail.com>
2025-04-10 12:41:49 +0200

    Delete js directory

--- js/barra.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    // Comprobar la posición del scroll
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector('.navbar').classList.add('scrolled');
-    } else {
-        document.querySelector('.navbar').classList.remove('scrolled');
-    }
-};
